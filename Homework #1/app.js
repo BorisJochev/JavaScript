b1a4e09f53e432cc1279db134f91ebf6259d655d
BorisJochev <119973977+BorisJochev@users.noreply.github.com>
2022-12-11 11:41:09 +0100

    Add files via upload

---
+++ Homework #1/app.js
@@ -0,0 +1,9 @@
+let phoneNum = 30;
+let price = 119.95;
+let texRate = 5;
+
+let tex = phoneNum * price * texRate / 100;
+let priceOfAll = phoneNum * price;
+let inTotal = priceOfAll + tex;
+
+console.log(inTotal)
