25a44cd8253a92d37d66fa9e367bcd20e00b95c0
BorisJochev <119973977+BorisJochev@users.noreply.github.com>
2022-12-11 20:12:03 +0100

    Add files via upload

---
+++ Homework #2/app.js
@@ -0,0 +1,36 @@
+
+
+let year = prompt("Vnesete godina na radjanje");
+let yearInput = parseInt(year);
+
+if(!Number.isNaN(yearInput)){
+    
+    if(year - 4 % 12 === 0){
+        console.log("Rat");
+    }else if((year - 4) % 12 === 1){
+        console.log("Ox")
+    }else if((year - 4) % 12 === 2){
+        console.log("Tiger")
+    }else if((year - 4) % 12 === 3){
+        console.log("Rabbit")
+    }else if((year - 4) % 12 === 4){
+        console.log("Dragon")
+    }else if((year - 4) % 12 === 5){
+        console.log("Snake")         
+    }else if((year - 4) % 12 === 6){
+        console.log("Horse")
+    }else if((year - 4) % 12 === 7){
+        console.log("Goat")
+    }else if((year - 4) % 12 === 8){
+        console.log("Monkey")
+    }else if((year - 4) % 12 === 9){
+        console.log("Rooster")
+    }else if((year - 4) % 12 === 10){
+        console.log("Dog")
+    }else if((year - 4) % 12 === 11){
+        console.log("Pig")
+    }}else{
+    console.log("Not a valid input")
+}
+
+
