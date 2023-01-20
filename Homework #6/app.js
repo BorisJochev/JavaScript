c0b7282ff757aeb97c8e8451c9cbd9deef12499a
BorisJochev <119973977+BorisJochev@users.noreply.github.com>
2023-01-20 20:17:09 +0100

    Add files via upload

---
+++ Homework #6/app.js
@@ -0,0 +1,19 @@
+
+// let name = prompt("Vnesete ime na Zivotnoto:")
+
+// let kind = prompt("Vnesete Zivotno:")
+
+// let speak = prompt("Vnesete kako zbor/krik na zivotnoto:")
+
+// let animal = {
+//     name : `${name}`,
+//     kind : `${kind}`,
+//     speak : `${speak}`,
+    
+//     speaker : function(){
+//         return `${this.kind} says: ${this.speak}!`;
+//     }
+// }
+
+
+
